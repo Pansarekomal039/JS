22e9d77d19423c22cf60a664e345d942c2ac1203
Pansarekomal039 <pansarekomal39@gmail.com>
2025-08-21 09:50:12 +0530

    Reverse the array

---
+++ reverse.js
@@ -0,0 +1,21 @@
+// var revstr = function(str){
+//     return str.split('').reverse().join('');
+// }
+function reverseArray(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+
+        left++;
+        right--;
+    }
+
+    return arr;
+}
+
+let arr = [1, 2, 3, 4, 5];
+console.log(reverseArray(arr));
